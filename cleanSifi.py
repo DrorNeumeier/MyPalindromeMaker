bb55eb1f1e66c181f1a9bf2b49ab302e4f96f224
zeevneumeier <zeev.neumeier@gmail.com>
2022-10-23 22:59:51 -0700

    sci

---
+++ cleanSifi.py
@@ -0,0 +1,75 @@
+import re
+
+dictionary_dir = "dictionaries/"
+
+f = open(dictionary_dir + "/internet_archive_scifi_v3.txt")
+
+lines = f.readlines()
+
+wordSet = {}
+sentenceSet = set()
+
+MIN_SENTENCE_WORD_COUNT = 3
+MIN_WORD_COUNT = 10
+
+for line in lines:
+    
+    sentences = re.split('[.!:;?#]+', line)
+    
+    for sentence in sentences:
+        sentence = re.sub(r'[^a-z ]', '', sentence.strip().lower())
+        
+        words = sentence.split(" ")
+        for word in words:
+            word = word.strip().lower()
+            word = re.sub(r'\W+', '', word)
+            
+            if len(word) > 0:
+                if word not in wordSet.keys():
+                    wordSet[word] = 0
+                wordSet[word] = wordSet[word] + 1
+        
+        if len(words) >= MIN_SENTENCE_WORD_COUNT:
+            sentenceSet.add(sentence)
+        
+print("before pruning")
+print(len(wordSet))
+print(len(sentenceSet))
+
+
+wordSet_ = set()
+for word in wordSet.keys():
+    if wordSet[word] >= MIN_WORD_COUNT:
+        wordSet_.add(word)
+wordSet = wordSet_
+
+sentenceSet_ = set()
+for sentence in sentenceSet:
+    words = set(sentence.split(" "))
+    
+    goodSentence = True
+    for word in words:
+        if word not in wordSet:
+            goodSentence = False
+            
+    if goodSentence:
+        sentenceSet_.add(sentence)
+sentenceSet = sentenceSet_
+    
+print("after pruning")
+print(len(wordSet))
+print(len(sentenceSet))
+
+f = open(dictionary_dir + "sci_words.txt", "w")
+for word in wordSet:
+    f.write("%s\n" %word)
+
+f.close()
+
+f = open(dictionary_dir + "sci_sentences.txt", "w")
+for sentense in sentenceSet:
+    f.write("%s\n" %sentense)
+
+f.close()
+
+
