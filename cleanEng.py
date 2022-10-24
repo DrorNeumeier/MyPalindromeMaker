e5b346222a69d32907a235ee5fc894358ed6194f
zeevneumeier <zeev.neumeier@gmail.com>
2022-10-23 23:46:02 -0700

    eng

---
+++ cleanEng.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import re
+
+dictionary_dir = "dictionaries/"
+
+df = pd.read_csv(dictionary_dir + "in_domain_train.tsv", delimiter='\t', header=None, names=['sentence_source', 'label', 'label_notes', 'sentence'])
+
+sentences = df.sentence.values
+labels = df.label.values
+
+sentences_ = []
+
+for i in range(len(sentences)):
+    if labels[i]:
+        sentences_.append(sentences[i])
+        
+print(len(sentences), len(sentences_))
+
+sentences = sentences_
+sentences_ = []
+words_ = set()
+
+for sentence in sentences:
+    sentence = re.sub(r'[^a-z ]', '', sentence.strip().lower())
+    sentences_.append(sentence)
+    
+    words = sentence.split(" ")
+    for word in words:
+        words_.add(word.strip().lower())
+        
+words = words_
+sentences = sentences_
+
+print(len(words), len(sentences))
+
+f = open(dictionary_dir + "eng_sentences.txt", "w")
+for sentense in sentences:
+    f.write("%s\n" %sentense)
+
+f.close()
+
+
