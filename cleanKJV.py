de2f4eefae5a080698986ffd10333d9c990b8aaf
zeevneumeier <zeev.neumeier@gmail.com>
2022-10-26 22:37:02 -0700

    kjv

--- cleanKJV.py
+++ cleanKJV.py
@@ -1,4 +1,5 @@
 import re
+import random
 
 dictionary_dir = "dictionaries"
 
@@ -6,47 +7,63 @@ f = open(dictionary_dir + "/kjv.txt")
 
 lines = f.readlines()
 
-kvj_wordSet = set()
-kvj_sentenseSet = set()
+words = set()
+sentences = set()
+sentencesPerProperNouns = {}
 
-MIN_SENTENCE_WORD_COUNT = 3
-MAX_SENTENCE_WORD_COUNT = 64
+MIN_WORD_PREVELANCE = 10
 
 for line in lines:
-    words = line.split(" ")[1:]
+    words_ = line.split(" ")[1:]
     
-    for word in words:
-        word = word.strip().lower()
-        word = re.sub(r'\W+', '', word)
-        
-        if len(word) > 0:
-          kvj_wordSet.add(word)
+    sentence = " ".join(words_)
+    sentence = re.sub(r'[^a-zA-Z ]', '', sentence.strip())
     
-    line = " ".join(words)
-    sentences = re.split('[.!:;?]+', line)
+    while "  " in sentence:
+        sentence = sentence.replace("  ", " ")
     
-    for sentense in sentences:
-      sentense = re.sub(r'[^a-z ]', '', sentense.strip().lower())
+    sentence = sentence.strip()
+    
+    
+    words_ = sentence.split(" ")
+    for word_i in range(len(words_)):
+        word = words_[word_i]
+        if word_i>0 and word[0].isupper():
+            #proper noun
+            if word not in sentencesPerProperNouns.keys():
+                sentencesPerProperNouns[word] = set()
+    
+            sentencesPerProperNouns[word].add(sentence)
+        else:
+            words.add(word.lower())
 
-      if sentense.count(" ") >= MIN_SENTENCE_WORD_COUNT-1 and sentense.count(" ") < MAX_SENTENCE_WORD_COUNT:
-        kvj_sentenseSet.add(sentense)
-        
-f = open(dictionary_dir + "/kjv_words.txt", "w")
-for word in kvj_wordSet:
-    if len(word) > 0:
-        f.write("%s\n" % word)
-    
+    sentences.add(sentence.lower())
+
+wordsLeftOut = 0
+for word in sentencesPerProperNouns.keys():
+    if len(sentencesPerProperNouns[word]) >= MIN_WORD_PREVELANCE:
+        words.add(word.lower())
+    else:
+        if word.lower() not in words:
+            wordsLeftOut = wordsLeftOut + 1
+            print(word, sentencesPerProperNouns[word])
+
+words = list(words)
+words.sort()
+
+print(len(sentences), "sentences and", len(words), "words with",wordsLeftOut, "proper nouns left out and",  len(lines), "lines")
+
+sentences = list(sentences)
+random.shuffle(sentences)
+
+f = open(dictionary_dir + "kjv_words.txt", "w")
+for word in words:
+    f.write("%s\n" %word)
+
 f.close()
 
-f = open(dictionary_dir + "/kjv_sentences.txt", "w")
-for sentense in kvj_sentenseSet:
-    if len(sentense) > 0:
-        f.write("%s\n" % sentense)
-    
+f = open(dictionary_dir + "kjv_sentences.txt", "w")
+for sentense in sentences:
+    f.write("%s\n" %sentense)
+
 f.close()
-
-print(len(kvj_wordSet))
-print(len(kvj_sentenseSet))
-
-print(list(kvj_wordSet)[:10])
-print(list(kvj_sentenseSet)[:10])
